679e9db9d5694b59eb1c8e4779ce35e280dee766
Andrew Shumeiko <shumeiko.drew@gmail.com>
2022-07-09 01:15:11 +0500

    feat: add opacity for screen-size < 912

--- script.js
+++ script.js
@@ -1,29 +1,51 @@
-const cards = document.querySelectorAll('.plan-card');
-const opacityCard = document.querySelector('.opacity');
-
-const getOpacity = () => {
-  cards.forEach((card) => {
-    card.addEventListener('mouseenter', () => {
-      if (card.classList.contains('opacity')) {
-        card.classList.remove('opacity');
-        opacityCard.classList.add('opacity');
-      } else {
-        opacityCard.classList.remove('opacity');
-        card.classList.add('opacity');
-      }
+document.addEventListener('DOMContentLoaded', () => {
+  const cards = document.querySelectorAll('.plan-card');
+  const opacityCard = document.querySelector('.opacity');
+  const getOpacity = () => {
+    cards.forEach((card) => {
+      card.addEventListener('mouseenter', () => {
+        if (card.classList.contains('opacity')) {
+          card.classList.remove('opacity');
+          opacityCard.classList.add('opacity');
+        } else {
+          opacityCard.classList.remove('opacity');
+          card.classList.add('opacity');
+        }
+      });
     });
-  });
-};
+  };
 
-const closeOpacity = () => {
-  cards.forEach((card) => {
-    card.addEventListener('mouseleave', () => {
-      if (card.classList.contains('opacity')) {
-        card.classList.remove('opacity');
-      }
+  const closeOpacity = () => {
+    cards.forEach((card) => {
+      card.addEventListener('mouseleave', () => {
+        if (card.classList.contains('opacity')) {
+          card.classList.remove('opacity');
+        }
+      });
     });
-  });
-};
+  };
 
-getOpacity();
-closeOpacity();
+  getOpacity();
+  closeOpacity();
+});
+
+/* click 1 */
+
+/* let activeCard = cards[0];
+  cards.forEach((card) => {
+    card.addEventListener('click', () => {
+      activeCard.classList.remove('opacity');
+      activeCard = card;
+      activeCard.classList.add('opacity');
+    });
+  }); */
+
+/* click 2 */
+
+/* cards.forEach((card) => {
+  card.addEventListener('click', function () {
+    cards.forEach((item) => item.classList.remove('opacity'));
+    this.classList.add('opacity');
+  });
+});
+ */
